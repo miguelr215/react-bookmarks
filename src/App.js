70ecebfaa5e2f5022355731a1dfb850080e309c9
drexelramos <drexelramos@gmail.com>
2020-10-30 11:09:40 -0700

    added api fetch

--- src/App.js
+++ src/App.js
@@ -3,20 +3,24 @@ import './app.css';
 import AddBookmark from './AddBookmark.js';
 import BookmarkApp from './BookmarkApp.js';
 
-const bookmarks = [
-  {
-    title: 'Google',
-    url: 'http://www.google.com',
-    rating: '3',
-    description: '50-50 evil'
-  },
-  {
-    title: 'ESPN',
-    url: 'http://www.espn.com',
-    rating: '2',
-    description: 'great content, terrible UI & UX'
-  }
-];
+// const bookmarks = [
+//   {
+//     title: 'Google',
+//     url: 'http://www.google.com',
+//     rating: '3',
+//     description: '50-50 evil'
+//   },
+//   {
+//     title: 'ESPN',
+//     url: 'http://www.espn.com',
+//     rating: '2',
+//     description: 'great content, terrible UI & UX'
+//   }
+// ];
+
+// bookmarks API key:  $2a$10$FDBbOeg4IS6b57HzJDD5XeQhJNe3SS6uBYaSERfZMrC3QMvjuHZP2
+// user name: mr215
+// pw: 7654321
 
 class App extends Component {
   constructor(props){
@@ -25,6 +29,37 @@ class App extends Component {
       bookmarks: [],
       showAddForm: false
     };
+  }
+
+  componentDidMount(){
+    const url = 'https://tf-ed-bookmarks-api.herokuapp.com/v3/bookmarks';
+    const options = {
+      method: 'GET',
+      headers: {
+        "Authorization": "Bearer $2a$10$FDBbOeg4IS6b57HzJDD5XeQhJNe3SS6uBYaSERfZMrC3QMvjuHZP2",
+        "Content-Type": "application/json"
+      }
+    };
+
+    fetch(url, options)
+      .then(response => {
+        if(!response.ok){
+          throw new Error('Something went wrong, please try again')
+        }
+        return response;
+      })
+      .then(response => response.json())
+      .then(data => {
+        this.setState({
+          bookmarks: data, 
+          error: null
+        })
+      })
+      .catch(err => {
+        this.setState({
+          error: err.message
+        })
+      });
   }
 
   render(){
